9ba8a4f4823dddc06ff9c14c9d413bce0a70b0af
Pasha <loban_pasha@mail.ru>
2022-03-18 13:54:16 +0300

    Правка

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 "use strict";
 let money = prompt('Ваш бюджет на месяц?','');
+
 let time = prompt('Введите дату в формате YYYY-MM-DD','');
+
 let question1 = prompt('Введите обязательную статью расходов в этом месяце','');
+
 let question2 = prompt('Во сколько обойдется?','');
+
 let appData = {
     budjet: money,
     timeData: time,
