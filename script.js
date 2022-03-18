e21731c2358828e3fe20da74f3608ae043e06786
Pasha <loban_pasha@mail.ru>
2022-03-18 13:57:33 +0300

    Probel

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let time = prompt('Введите дату в формате YYYY-MM-DD','');
 let question1 = prompt('Введите обязательную статью расходов в этом месяце','');
 
 let question2 = prompt('Во сколько обойдется?','');
-
 let appData = {
     budjet: money,
     timeData: time,
